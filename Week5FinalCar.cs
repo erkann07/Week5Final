6cc2d75830691d80ca73d89e8e2e360ee29321e4
Erkan <erkankrgz0707@gmail.com>
2025-06-23 15:13:40 +0300

    Add files via upload

---
+++ Week5FinalCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Final
+{
+    internal class Car
+    {
+        public DateTime UretimTarihi { get; set; }
+        public int SeriNo { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+        public int KapiSayisi { get; set; }
+    }
+}
---
+++ Week5FinalProgram.cs
@@ -0,0 +1,77 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Week5Final;
+using Car = Week5Final.Car;
+class Program
+{
+    static void Main()
+    {
+    giris:
+        Console.WriteLine("Araba üretmek istiyor musunuz ?(E/H)");
+        string cevap = Console.ReadLine();
+        cevap = cevap.Trim().ToUpper();
+        List<Car> arabalar = new List<Car>();
+
+        if (cevap == "E")
+        {
+        newCar1:
+            Car newCar = new Car();
+            newCar.UretimTarihi = DateTime.Now;
+            Console.Write("Seri No:");
+            newCar.SeriNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Marka:");
+            newCar.Marka = Console.ReadLine();
+            Console.Write("Model:");
+            newCar.Model = Console.ReadLine();
+            Console.Write("Renk:");
+            newCar.Renk = Console.ReadLine();
+        kapiSayi:
+            Console.Write("Kapı Sayısı:");
+            string kapiGiris1 = Console.ReadLine();
+            if (int.TryParse(kapiGiris1, out int kapiSayi))
+            {
+                newCar.KapiSayisi = kapiSayi;
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir kapı sayısı giriniz.");
+                goto kapiSayi;
+            }
+            arabalar.Add(newCar);
+            Console.WriteLine("Başka araba üretmek istiyor musunuz ?(E/H)");
+        cevap2Giris:
+            string cevap2 = Console.ReadLine();
+            cevap2 = cevap2.Trim().ToUpper();
+            if (cevap2 == "E")
+            {
+                goto newCar1;
+            }
+            else if (cevap2 == "H")
+            {
+                Console.WriteLine("\n------- ÜRETİLEN ARAÇLAR -------");
+                foreach (var car in arabalar)
+                {
+                    Console.WriteLine($"Seri NO: {car.SeriNo} , Marka: {car.Marka}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lütfen E veya H harflerinden birini giriniz.");
+                goto cevap2Giris;
+            }
+        }
+        else if (cevap == "H")
+        {
+            Console.WriteLine("Programdan çıkılıyor...");
+        }
+        else
+        {
+            Console.WriteLine("Lütfen E veya H harflerinden birini giriniz.");
+            goto giris;
+        }
+    }
+}
+
